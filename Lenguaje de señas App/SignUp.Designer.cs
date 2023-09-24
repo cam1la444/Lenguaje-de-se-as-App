2d4602bb7d0930f47e0fdd8c5d05a4b7b113a097
manci <manci@LAPTOP-S0L1KC73>
2023-09-24 15:00:24 -0600

    Agregue un button para identificador de Crear Cuenta

--- Lenguaje de señas App/SignUp.Designer.cs
+++ Lenguaje de señas App/SignUp.Designer.cs
@@ -33,12 +33,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 264);
+            this.label1.Location = new System.Drawing.Point(45, 105);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 0;
@@ -46,7 +47,7 @@
             // 
             // txtCrearUsuario
             // 
-            this.txtCrearUsuario.Location = new System.Drawing.Point(94, 261);
+            this.txtCrearUsuario.Location = new System.Drawing.Point(103, 102);
             this.txtCrearUsuario.Name = "txtCrearUsuario";
             this.txtCrearUsuario.Size = new System.Drawing.Size(284, 20);
             this.txtCrearUsuario.TabIndex = 1;
@@ -54,7 +55,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 317);
+            this.label2.Location = new System.Drawing.Point(27, 167);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 2;
@@ -62,7 +63,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(94, 314);
+            this.textBox1.Location = new System.Drawing.Point(103, 167);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(284, 20);
             this.textBox1.TabIndex = 3;
@@ -70,17 +71,28 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(216, 168);
+            this.label3.Font = new System.Drawing.Font("MV Boli", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(166, 26);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.Size = new System.Drawing.Size(164, 39);
             this.label3.TabIndex = 4;
             this.label3.Text = "Registrate";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Crear Cuenta";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // SignUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(418, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -100,5 +112,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
