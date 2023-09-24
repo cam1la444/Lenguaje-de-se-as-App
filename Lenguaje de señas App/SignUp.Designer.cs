9bd214862a3b17b8b3a30a12824fed8be7484fee
manci <manci@LAPTOP-S0L1KC73>
2023-09-24 14:51:43 -0600

    Agregue un textbox para identificador de crear usuario

--- Lenguaje de señas App/SignUp.Designer.cs
+++ Lenguaje de señas App/SignUp.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.txtCrearUsuario = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,19 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario";
             // 
+            // txtCrearUsuario
+            // 
+            this.txtCrearUsuario.Location = new System.Drawing.Point(182, 57);
+            this.txtCrearUsuario.Name = "txtCrearUsuario";
+            this.txtCrearUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtCrearUsuario.TabIndex = 1;
+            // 
             // SignUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCrearUsuario);
             this.Controls.Add(this.label1);
             this.Name = "SignUp";
             this.Text = "SignUp";
@@ -56,5 +65,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCrearUsuario;
     }
 }
