337f33e8d1d9112f67062026d5c78f0084954a95
cam1la444 <camila24melara@gmail.com>
2023-09-24 16:50:43 -0600

    Agrando codigo para el video player de la A

--- Lenguaje de señas App/LetraA.Designer.cs
+++ Lenguaje de señas App/LetraA.Designer.cs
@@ -32,6 +32,7 @@
             this.btnRegresarA = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnReproducir = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
@@ -68,12 +69,23 @@
             this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(222, 192);
             this.axWindowsMediaPlayer1.TabIndex = 2;
             // 
+            // btnReproducir
+            // 
+            this.btnReproducir.Location = new System.Drawing.Point(329, 325);
+            this.btnReproducir.Name = "btnReproducir";
+            this.btnReproducir.Size = new System.Drawing.Size(75, 23);
+            this.btnReproducir.TabIndex = 3;
+            this.btnReproducir.Text = "button1";
+            this.btnReproducir.UseVisualStyleBackColor = true;
+            this.btnReproducir.Click += new System.EventHandler(this.btnReproducir_Click);
+            // 
             // LetraA
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(341, 443);
+            this.ClientSize = new System.Drawing.Size(400, 457);
+            this.Controls.Add(this.btnReproducir);
             this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnRegresarA);
@@ -92,5 +104,6 @@
         private System.Windows.Forms.Button btnRegresarA;
         private System.Windows.Forms.PictureBox pictureBox1;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btnReproducir;
     }
 }
