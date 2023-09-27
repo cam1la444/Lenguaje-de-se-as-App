6a6b20ea74e2f62f8c50bf526d6cc613766943b6
cam1la444 <camila24melara@gmail.com>
2023-09-26 22:04:37 -0600

    Agreagando formlarios para inicio, numeros, y del 0 al 10, 20-100

---
+++ Lenguaje de señas App/Numero20.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lenguaje_de_señas_App
+{
+    public partial class Numero20 : Form
+    {
+        public Numero20()
+        {
+            InitializeComponent();
+        }
+    }
+}
