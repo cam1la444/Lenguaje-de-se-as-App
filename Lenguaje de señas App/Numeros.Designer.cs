fbecec39030261ac853cbdb4fc5d379835b0e5f1
floxyz <florgxyz@gmail.com>
2023-10-17 17:32:47 -0600

    El mismo color a las ventanas principales

--- Lenguaje de señas App/Numeros.Designer.cs
+++ Lenguaje de señas App/Numeros.Designer.cs
@@ -299,6 +299,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(230)))), ((int)(((byte)(241)))));
             this.ClientSize = new System.Drawing.Size(790, 442);
             this.Controls.Add(this.button21);
             this.Controls.Add(this.btn1);
