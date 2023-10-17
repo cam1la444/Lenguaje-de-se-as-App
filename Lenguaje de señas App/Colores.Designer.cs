89d8ddfe307222fde4f117a3e4e972a6d1a409b9
manci <manci@LAPTOP-S0L1KC73>
2023-10-17 16:28:12 -0600

    Agregue 2 button para rojo y negro

--- Lenguaje de señas App/Colores.Designer.cs
+++ Lenguaje de señas App/Colores.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRojo = new System.Windows.Forms.Button();
+            this.btnNegro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRojo
+            // 
+            this.btnRojo.BackColor = System.Drawing.Color.Red;
+            this.btnRojo.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRojo.Location = new System.Drawing.Point(13, 13);
+            this.btnRojo.Name = "btnRojo";
+            this.btnRojo.Size = new System.Drawing.Size(136, 51);
+            this.btnRojo.TabIndex = 0;
+            this.btnRojo.Text = "Rojo";
+            this.btnRojo.UseVisualStyleBackColor = false;
+            // 
+            // btnNegro
+            // 
+            this.btnNegro.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btnNegro.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNegro.ForeColor = System.Drawing.Color.White;
+            this.btnNegro.Location = new System.Drawing.Point(156, 13);
+            this.btnNegro.Name = "btnNegro";
+            this.btnNegro.Size = new System.Drawing.Size(132, 51);
+            this.btnNegro.TabIndex = 1;
+            this.btnNegro.Text = "Negro";
+            this.btnNegro.UseVisualStyleBackColor = false;
+            // 
+            // Colores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNegro);
+            this.Controls.Add(this.btnRojo);
+            this.Name = "Colores";
             this.Text = "Colores";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRojo;
+        private System.Windows.Forms.Button btnNegro;
     }
 }
