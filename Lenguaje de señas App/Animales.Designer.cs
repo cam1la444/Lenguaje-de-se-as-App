cd2c1e600ce3ad9a0d9c03e9fa095ec80660bbba
manci <manci@LAPTOP-S0L1KC73>
2023-10-17 11:37:00 -0600

    Agregue un boton para Perro

---
+++ Lenguaje de señas App/Animales.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Lenguaje_de_señas_App
+{
+    partial class Animales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPerro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPerro
+            // 
+            this.btnPerro.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPerro.Location = new System.Drawing.Point(13, 13);
+            this.btnPerro.Name = "btnPerro";
+            this.btnPerro.Size = new System.Drawing.Size(135, 51);
+            this.btnPerro.TabIndex = 0;
+            this.btnPerro.Text = "Perro";
+            this.btnPerro.UseVisualStyleBackColor = true;
+            // 
+            // Animales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPerro);
+            this.Name = "Animales";
+            this.Text = "Animales";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPerro;
+    }
+}
