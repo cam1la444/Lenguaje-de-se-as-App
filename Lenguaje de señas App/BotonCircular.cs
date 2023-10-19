acdf1b90be4855af08415600364c0f2b67bfa514
cam1la444 <camila24melara@gmail.com>
2023-10-18 18:19:39 -0600

    Agg clase para botones en colores

---
+++ Lenguaje de señas App/BotonCircular.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+
+namespace Lenguaje_de_señas_App
+{
+    internal class BotonCircular:Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+           GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(path);
+            base.OnPaint(pevent);
+        }
+    }
+}
--- Lenguaje de señas App/Colores.Designer.cs
+++ Lenguaje de señas App/Colores.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Colores));
             this.btnRojo = new System.Windows.Forms.Button();
             this.btnNegro = new System.Windows.Forms.Button();
             this.btnBlanco = new System.Windows.Forms.Button();
@@ -37,10 +38,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnRojo
@@ -48,11 +50,12 @@
             this.btnRojo.BackColor = System.Drawing.Color.Red;
             this.btnRojo.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnRojo.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRojo.Location = new System.Drawing.Point(13, 13);
+            this.btnRojo.Location = new System.Drawing.Point(42, 148);
             this.btnRojo.Name = "btnRojo";
-            this.btnRojo.Size = new System.Drawing.Size(136, 51);
+            this.btnRojo.Size = new System.Drawing.Size(108, 51);
             this.btnRojo.TabIndex = 0;
             this.btnRojo.Text = "Rojo";
+            this.btnRojo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnRojo.UseVisualStyleBackColor = false;
             // 
             // btnNegro
@@ -61,11 +64,12 @@
             this.btnNegro.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnNegro.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNegro.ForeColor = System.Drawing.Color.White;
-            this.btnNegro.Location = new System.Drawing.Point(182, 12);
+            this.btnNegro.Location = new System.Drawing.Point(228, 147);
             this.btnNegro.Name = "btnNegro";
             this.btnNegro.Size = new System.Drawing.Size(136, 51);
             this.btnNegro.TabIndex = 1;
             this.btnNegro.Text = "Negro";
+            this.btnNegro.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnNegro.UseVisualStyleBackColor = false;
             // 
             // btnBlanco
@@ -73,11 +77,12 @@
             this.btnBlanco.BackColor = System.Drawing.Color.White;
             this.btnBlanco.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnBlanco.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBlanco.Location = new System.Drawing.Point(366, 12);
+            this.btnBlanco.Location = new System.Drawing.Point(425, 147);
             this.btnBlanco.Name = "btnBlanco";
             this.btnBlanco.Size = new System.Drawing.Size(136, 51);
             this.btnBlanco.TabIndex = 2;
             this.btnBlanco.Text = "Blanco";
+            this.btnBlanco.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnBlanco.UseVisualStyleBackColor = false;
             // 
             // btnAzul
@@ -85,11 +90,12 @@
             this.btnAzul.BackColor = System.Drawing.Color.Blue;
             this.btnAzul.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAzul.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAzul.Location = new System.Drawing.Point(533, 12);
+            this.btnAzul.Location = new System.Drawing.Point(610, 147);
             this.btnAzul.Name = "btnAzul";
             this.btnAzul.Size = new System.Drawing.Size(136, 51);
             this.btnAzul.TabIndex = 3;
             this.btnAzul.Text = "Azul";
+            this.btnAzul.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnAzul.UseVisualStyleBackColor = false;
             // 
             // btnAmarillo
@@ -97,11 +103,12 @@
             this.btnAmarillo.BackColor = System.Drawing.Color.Yellow;
             this.btnAmarillo.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAmarillo.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAmarillo.Location = new System.Drawing.Point(13, 96);
+            this.btnAmarillo.Location = new System.Drawing.Point(42, 231);
             this.btnAmarillo.Name = "btnAmarillo";
             this.btnAmarillo.Size = new System.Drawing.Size(136, 51);
             this.btnAmarillo.TabIndex = 4;
             this.btnAmarillo.Text = "Amarillo";
+            this.btnAmarillo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnAmarillo.UseVisualStyleBackColor = false;
             // 
             // btnRosado
@@ -109,11 +116,12 @@
             this.btnRosado.BackColor = System.Drawing.Color.Pink;
             this.btnRosado.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnRosado.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRosado.Location = new System.Drawing.Point(182, 96);
+            this.btnRosado.Location = new System.Drawing.Point(228, 231);
             this.btnRosado.Name = "btnRosado";
             this.btnRosado.Size = new System.Drawing.Size(136, 51);
             this.btnRosado.TabIndex = 5;
             this.btnRosado.Text = "Rosado";
+            this.btnRosado.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnRosado.UseVisualStyleBackColor = false;
             // 
             // button1
@@ -121,11 +129,12 @@
             this.button1.BackColor = System.Drawing.Color.Green;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(366, 96);
+            this.button1.Location = new System.Drawing.Point(425, 231);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(136, 51);
             this.button1.TabIndex = 6;
             this.button1.Text = "Verde";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
@@ -133,11 +142,12 @@
             this.button2.BackColor = System.Drawing.Color.Brown;
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(533, 96);
+            this.button2.Location = new System.Drawing.Point(610, 231);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(136, 51);
             this.button2.TabIndex = 7;
             this.button2.Text = "Café";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
@@ -145,35 +155,24 @@
             this.button3.BackColor = System.Drawing.Color.SkyBlue;
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button3.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(13, 182);
+            this.button3.Location = new System.Drawing.Point(42, 317);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(136, 51);
             this.button3.TabIndex = 8;
             this.button3.Text = "Celeste";
             this.button3.UseVisualStyleBackColor = false;
             // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.SkyBlue;
-            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(13, 277);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(136, 51);
-            this.button4.TabIndex = 9;
-            this.button4.Text = "Celeste";
-            this.button4.UseVisualStyleBackColor = false;
-            // 
             // button5
             // 
             this.button5.BackColor = System.Drawing.Color.SkyBlue;
             this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button5.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(366, 182);
+            this.button5.Location = new System.Drawing.Point(425, 317);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(136, 51);
             this.button5.TabIndex = 10;
-            this.button5.Text = "Celeste";
+            this.button5.Text = "Anaranjado";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button5.UseVisualStyleBackColor = false;
             // 
             // button6
@@ -181,11 +180,12 @@
             this.button6.BackColor = System.Drawing.Color.SkyBlue;
             this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button6.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(533, 182);
+            this.button6.Location = new System.Drawing.Point(610, 317);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(136, 51);
             this.button6.TabIndex = 11;
-            this.button6.Text = "Celeste";
+            this.button6.Text = "Gris";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button6.UseVisualStyleBackColor = false;
             // 
             // button7
@@ -193,12 +193,23 @@
             this.button7.BackColor = System.Drawing.Color.SkyBlue;
             this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button7.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(182, 182);
+            this.button7.Location = new System.Drawing.Point(228, 317);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(136, 51);
             this.button7.TabIndex = 12;
-            this.button7.Text = "Celeste";
+            this.button7.Text = "Morado";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.button7.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(150, 148);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(47, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
             // 
             // Colores
             // 
@@ -206,10 +217,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(230)))), ((int)(((byte)(241)))));
             this.ClientSize = new System.Drawing.Size(790, 442);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -222,6 +233,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Colores";
             this.Text = "Colores";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -237,9 +249,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
