5b4bfc4190a3b14683c402db14197b808f3cda74
cam1la444 <camila24melara@gmail.com>
2023-10-19 23:00:38 -0600

    Codigo correcto para +info y abtde, 95% correcto program(6forms)

--- Lenguaje de señas App/Incicio.cs
+++ Lenguaje de señas App/Incicio.cs
@@ -109,14 +109,14 @@ namespace Lenguaje_de_señas_App
         private void btnMasInforms_Click(object sender, EventArgs e)
         {
             MasInformacion masinfo = new MasInformacion();
-            this.Close();
+            this.Hide();
             masinfo.Show();
         }
 
         private void btnAcercade_Click(object sender, EventArgs e)
         {
             Acercade acercade = new Acercade();
-            this.Close();
+            this.Hide();
             acercade.Show();
         }
     }
