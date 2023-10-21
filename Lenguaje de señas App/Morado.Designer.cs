6df46f2c834eae90af3995c1f6b9af78532bd3af
manci <manci@LAPTOP-S0L1KC73>
2023-10-21 14:21:46 -0600

    le cambie el color a un button

--- Lenguaje de señas App/Morado.Designer.cs
+++ Lenguaje de señas App/Morado.Designer.cs
@@ -34,12 +34,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Khaki;
             this.button1.Location = new System.Drawing.Point(273, 200);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 47);
             this.button1.TabIndex = 0;
             this.button1.Text = "Reproducir video";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
