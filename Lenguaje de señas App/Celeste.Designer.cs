5f459728d7fd7997e0d09ad6aba5e0e645367ec0
manci <manci@LAPTOP-S0L1KC73>
2023-10-21 15:01:24 -0600

    Add un button para Reproducir video y le cambien color

--- Lenguaje de señas App/Celeste.Designer.cs
+++ Lenguaje de señas App/Celeste.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Khaki;
+            this.button1.Location = new System.Drawing.Point(324, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Reproducir video";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Celeste
             // 
@@ -36,6 +47,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(420, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Celeste";
             this.Text = "Celeste";
             this.ResumeLayout(false);
@@ -43,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
