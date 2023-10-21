9989104b2431a23a94766f7c99197bc12a71d873
manci <manci@LAPTOP-S0L1KC73>
2023-10-21 15:04:37 -0600

    Cambie el tamaño de formulario

--- Lenguaje de señas App/Blanco.Designer.cs
+++ Lenguaje de señas App/Blanco.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Blanco
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(399, 450);
+            this.Name = "Blanco";
             this.Text = "Blanco";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
