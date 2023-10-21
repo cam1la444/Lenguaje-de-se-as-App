f90f60ff098c6e06fee3a260febcf8cb8f7ff0c1
manci <manci@LAPTOP-S0L1KC73>
2023-10-21 17:00:36 -0600

    Add color a Form Verde

--- Lenguaje de señas App/Verde.Designer.cs
+++ Lenguaje de señas App/Verde.Designer.cs
@@ -69,6 +69,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(391, 450);
             this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.btnReproducir);
