0db95995606ef1de41aa34982d5f9715e5596c53
manci <manci@LAPTOP-S0L1KC73>
2023-10-21 16:13:02 -0600

    Add name btnAzul

--- Lenguaje de señas App/Colores.Designer.cs
+++ Lenguaje de señas App/Colores.Designer.cs
@@ -40,7 +40,7 @@
             this.btnMorado = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.btnAzul = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
@@ -193,15 +193,15 @@
             this.button5.Text = "Amarillo";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnAzul
             // 
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(196, 199);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(81, 44);
-            this.button6.TabIndex = 103;
-            this.button6.Text = "Azul";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnAzul.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAzul.Location = new System.Drawing.Point(196, 199);
+            this.btnAzul.Name = "btnAzul";
+            this.btnAzul.Size = new System.Drawing.Size(81, 44);
+            this.btnAzul.TabIndex = 103;
+            this.btnAzul.Text = "Azul";
+            this.btnAzul.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
@@ -286,7 +286,7 @@
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btnAzul);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.btnMorado);
@@ -321,7 +321,7 @@
         private System.Windows.Forms.Button btnMorado;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnAzul;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
