8e09fa5721e9129f8d856c575cd4e76e51d9def6
manci <manci@LAPTOP-S0L1KC73>
2023-10-21 16:01:00 -0600

    Add name btnReproducir

--- Lenguaje de señas App/Celeste.Designer.cs
+++ Lenguaje de señas App/Celeste.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Celeste));
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnReproducir = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnReproducir
             // 
-            this.button1.BackColor = System.Drawing.Color.Khaki;
-            this.button1.Location = new System.Drawing.Point(324, 186);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 66);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Reproducir video";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnReproducir.BackColor = System.Drawing.Color.Khaki;
+            this.btnReproducir.Location = new System.Drawing.Point(324, 186);
+            this.btnReproducir.Name = "btnReproducir";
+            this.btnReproducir.Size = new System.Drawing.Size(75, 66);
+            this.btnReproducir.TabIndex = 0;
+            this.btnReproducir.Text = "Reproducir video";
+            this.btnReproducir.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
@@ -73,7 +73,7 @@
             this.ClientSize = new System.Drawing.Size(420, 450);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnReproducir);
             this.Name = "Celeste";
             this.Text = "Celeste";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -83,7 +83,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnReproducir;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
