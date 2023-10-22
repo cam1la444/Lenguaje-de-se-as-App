d78e2fa05b47a8bbd41768c12a9d270a6b504bf6
manci <manci@LAPTOP-S0L1KC73>
2023-10-22 16:44:17 -0600

    Add picture box

--- Lenguaje de señas App/Marañon.Designer.cs
+++ Lenguaje de señas App/Marañon.Designer.cs
@@ -36,7 +36,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(123, 26);
+            this.pictureBox1.Location = new System.Drawing.Point(126, 21);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(136, 136);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
