d1da0ca4d560b430f2357c8d14277c54bf184a5b
manci <manci@LAPTOP-S0L1KC73>
2023-10-22 17:52:45 -0600

    add imagen

--- Lenguaje de señas App/Mamones.Designer.cs
+++ Lenguaje de señas App/Mamones.Designer.cs
@@ -58,9 +58,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(143, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(164, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(136, 136);
+            this.pictureBox1.Size = new System.Drawing.Size(120, 139);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
