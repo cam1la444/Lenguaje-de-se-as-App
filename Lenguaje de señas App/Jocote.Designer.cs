3db45cf805edbdc0091d5139344cf2e2ad29ffd2
manci <manci@LAPTOP-S0L1KC73>
2023-10-22 16:52:34 -0600

    Add form Jocote

---
+++ Lenguaje de señas App/Jocote.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Lenguaje_de_señas_App
+{
+    partial class Jocote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Jocote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(230)))), ((int)(((byte)(241)))));
+            this.ClientSize = new System.Drawing.Size(384, 450);
+            this.Name = "Jocote";
+            this.Text = "Jocote";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
