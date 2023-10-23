a45e838419d62832990b0b20fc629805a606270c
cam1la444 <camila24melara@gmail.com>
2023-10-23 16:47:02 -0600

    agg video para ayer, esta noche y domingo

--- Lenguaje de señas App/Esta Noche.cs
+++ Lenguaje de señas App/Esta Noche.cs
@@ -16,5 +16,16 @@ namespace Lenguaje_de_señas_App
         {
             InitializeComponent();
         }
+
+        private void btnReproducir_Click(object sender, EventArgs e)
+        {
+            string Tonight = "C:\\Users\\jwest\\Videos\\VideoTonight.mp4";
+            asWMPA.URL = Tonight;
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
