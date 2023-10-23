603fd6aa6d3f8f5173f0916c6f47635303f9664c
manci <manci@LAPTOP-S0L1KC73>
2023-10-22 23:28:09 -0600

    add form camisa

---
+++ Lenguaje de señas App/Camisa.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Lenguaje_de_señas_App
+{
+    partial class Camisa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Camisa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(385, 450);
+            this.Name = "Camisa";
+            this.Text = "Camisa";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
