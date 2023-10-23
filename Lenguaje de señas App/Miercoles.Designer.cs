1a2fb917cbced2c2289c8a0efdc5eae9c2408b4d
manci <manci@LAPTOP-S0L1KC73>
2023-10-22 19:17:55 -0600

    Add regresar

--- Lenguaje de señas App/Miercoles.Designer.cs
+++ Lenguaje de señas App/Miercoles.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnReproducir = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnReproducir
@@ -41,11 +42,22 @@
             this.btnReproducir.Text = "Reproducir Video";
             this.btnReproducir.UseVisualStyleBackColor = false;
             // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnRegresar.Location = new System.Drawing.Point(145, 399);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(102, 39);
+            this.btnRegresar.TabIndex = 5;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            // 
             // Miercoles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(406, 450);
+            this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.btnReproducir);
             this.Name = "Miercoles";
             this.Text = "Miercoles";
@@ -56,5 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnReproducir;
+        private System.Windows.Forms.Button btnRegresar;
     }
 }
