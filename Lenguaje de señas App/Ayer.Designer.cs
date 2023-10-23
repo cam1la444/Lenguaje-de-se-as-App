6649f7c5cf1027f13c5262b2e392f8b51871ef1a
manci <manci@LAPTOP-S0L1KC73>
2023-10-22 19:52:29 -0600

    add btn

--- Lenguaje de señas App/Ayer.Designer.cs
+++ Lenguaje de señas App/Ayer.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnRegresar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnRegresar.Location = new System.Drawing.Point(147, 399);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(102, 39);
+            this.btnRegresar.TabIndex = 5;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
             // 
             // Ayer
             // 
@@ -36,6 +47,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.ClientSize = new System.Drawing.Size(416, 450);
+            this.Controls.Add(this.btnRegresar);
             this.Name = "Ayer";
             this.Text = "Ayer";
             this.ResumeLayout(false);
@@ -43,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRegresar;
     }
 }
