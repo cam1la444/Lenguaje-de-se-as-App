4aea4eab198a6baad1b85e6840c137c69535785f
manci <manci@LAPTOP-S0L1KC73>
2023-10-22 18:55:31 -0600

    Add btn

--- Lenguaje de señas App/Hoy.Designer.cs
+++ Lenguaje de señas App/Hoy.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnReproducir = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnReproducir
@@ -41,12 +42,23 @@
             this.btnReproducir.Text = "Reproducir Video";
             this.btnReproducir.UseVisualStyleBackColor = false;
             // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnRegresar.Location = new System.Drawing.Point(128, 399);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(102, 39);
+            this.btnRegresar.TabIndex = 3;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            // 
             // Hoy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(230)))), ((int)(((byte)(241)))));
             this.ClientSize = new System.Drawing.Size(401, 450);
+            this.Controls.Add(this.btnRegresar);
             this.Controls.Add(this.btnReproducir);
             this.Name = "Hoy";
             this.Text = "Hoy";
@@ -57,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnReproducir;
+        private System.Windows.Forms.Button btnRegresar;
     }
 }
