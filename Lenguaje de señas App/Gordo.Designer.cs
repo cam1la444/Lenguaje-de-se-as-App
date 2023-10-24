86349660f2437310c78317af5ef8e04716ade25e
manci <manci@LAPTOP-S0L1KC73>
2023-10-23 19:54:23 -0600

    Add formularios

---
+++ Lenguaje de señas App/Gordo.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Lenguaje_de_señas_App
+{
+    partial class Gordo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Gordo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(415, 450);
+            this.Name = "Gordo";
+            this.Text = "Gordo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
