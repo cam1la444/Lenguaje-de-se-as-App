d00c6ac8c88271f36d01f2a7633460bbc7f697ff
manci <manci@LAPTOP-S0L1KC73>
2023-10-23 20:27:30 -0600

    Add nombres a otones

--- Lenguaje de señas App/Saludos.Designer.cs
+++ Lenguaje de señas App/Saludos.Designer.cs
@@ -36,7 +36,7 @@
             this.btnLecciones = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.btnHoy = new System.Windows.Forms.Button();
+            this.btnHola = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -47,8 +47,8 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.button12 = new System.Windows.Forms.Button();
+            this.btnBien = new System.Windows.Forms.Button();
+            this.btnComoestas = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -154,20 +154,20 @@
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
-            // btnHoy
+            // btnHola
             // 
-            this.btnHoy.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnHoy.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
-            this.btnHoy.FlatAppearance.BorderSize = 2;
-            this.btnHoy.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.btnHoy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHoy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.btnHoy.Location = new System.Drawing.Point(26, 58);
-            this.btnHoy.Name = "btnHoy";
-            this.btnHoy.Size = new System.Drawing.Size(135, 51);
-            this.btnHoy.TabIndex = 69;
-            this.btnHoy.Text = "Hoy";
-            this.btnHoy.UseVisualStyleBackColor = true;
+            this.btnHola.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHola.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnHola.FlatAppearance.BorderSize = 2;
+            this.btnHola.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnHola.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHola.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnHola.Location = new System.Drawing.Point(26, 58);
+            this.btnHola.Name = "btnHola";
+            this.btnHola.Size = new System.Drawing.Size(135, 51);
+            this.btnHola.TabIndex = 69;
+            this.btnHola.Text = "Hola";
+            this.btnHola.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
@@ -179,9 +179,9 @@
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.button1.Location = new System.Drawing.Point(252, 148);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 51);
+            this.button1.Size = new System.Drawing.Size(135, 61);
             this.button1.TabIndex = 70;
-            this.button1.Text = "Hoy";
+            this.button1.Text = "¿Cómo estás tú?";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -194,9 +194,9 @@
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.button2.Location = new System.Drawing.Point(643, 148);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 51);
+            this.button2.Size = new System.Drawing.Size(135, 61);
             this.button2.TabIndex = 71;
-            this.button2.Text = "Hoy";
+            this.button2.Text = "Buenas tardes";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -207,11 +207,11 @@
             this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button3.Location = new System.Drawing.Point(450, 323);
+            this.button3.Location = new System.Drawing.Point(450, 314);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(135, 51);
             this.button3.TabIndex = 72;
-            this.button3.Text = "Hoy";
+            this.button3.Text = "Cuidate";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -226,7 +226,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(135, 51);
             this.button4.TabIndex = 73;
-            this.button4.Text = "Hoy";
+            this.button4.Text = "Adiós";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -239,9 +239,9 @@
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.button5.Location = new System.Drawing.Point(349, 227);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(135, 51);
+            this.button5.Size = new System.Drawing.Size(135, 61);
             this.button5.TabIndex = 74;
-            this.button5.Text = "Hoy";
+            this.button5.Text = "Mucho gusto";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
@@ -254,9 +254,9 @@
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.button6.Location = new System.Drawing.Point(450, 148);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(135, 51);
+            this.button6.Size = new System.Drawing.Size(135, 61);
             this.button6.TabIndex = 75;
-            this.button6.Text = "Hoy";
+            this.button6.Text = "Buenos dias";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
@@ -271,7 +271,7 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(135, 51);
             this.button7.TabIndex = 76;
-            this.button7.Text = "Hoy";
+            this.button7.Text = "Por favor";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
@@ -284,9 +284,9 @@
             this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
             this.button8.Location = new System.Drawing.Point(129, 227);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(135, 51);
+            this.button8.Size = new System.Drawing.Size(135, 61);
             this.button8.TabIndex = 77;
-            this.button8.Text = "Hoy";
+            this.button8.Text = "Buenas noches";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button9
@@ -301,7 +301,7 @@
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(135, 51);
             this.button9.TabIndex = 78;
-            this.button9.Text = "Hoy";
+            this.button9.Text = "Gracias";
             this.button9.UseVisualStyleBackColor = true;
             // 
             // button10
@@ -316,46 +316,46 @@
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(135, 51);
             this.button10.TabIndex = 79;
-            this.button10.Text = "Hoy";
+            this.button10.Text = "Mal";
             this.button10.UseVisualStyleBackColor = true;
             // 
-            // button11
+            // btnBien
             // 
-            this.button11.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button11.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
-            this.button11.FlatAppearance.BorderSize = 2;
-            this.button11.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button11.Location = new System.Drawing.Point(392, 58);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(135, 51);
-            this.button11.TabIndex = 80;
-            this.button11.Text = "Hoy";
-            this.button11.UseVisualStyleBackColor = true;
+            this.btnBien.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBien.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnBien.FlatAppearance.BorderSize = 2;
+            this.btnBien.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnBien.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBien.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnBien.Location = new System.Drawing.Point(392, 58);
+            this.btnBien.Name = "btnBien";
+            this.btnBien.Size = new System.Drawing.Size(135, 51);
+            this.btnBien.TabIndex = 80;
+            this.btnBien.Text = "Bien";
+            this.btnBien.UseVisualStyleBackColor = true;
             // 
-            // button12
+            // btnComoestas
             // 
-            this.button12.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button12.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
-            this.button12.FlatAppearance.BorderSize = 2;
-            this.button12.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button12.Location = new System.Drawing.Point(206, 58);
-            this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(135, 51);
-            this.button12.TabIndex = 81;
-            this.button12.Text = "Hoy";
-            this.button12.UseVisualStyleBackColor = true;
+            this.btnComoestas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnComoestas.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnComoestas.FlatAppearance.BorderSize = 2;
+            this.btnComoestas.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnComoestas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnComoestas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnComoestas.Location = new System.Drawing.Point(206, 58);
+            this.btnComoestas.Name = "btnComoestas";
+            this.btnComoestas.Size = new System.Drawing.Size(152, 64);
+            this.btnComoestas.TabIndex = 81;
+            this.btnComoestas.Text = "¿Como estás?";
+            this.btnComoestas.UseVisualStyleBackColor = true;
             // 
             // Saludos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(790, 442);
-            this.Controls.Add(this.button12);
-            this.Controls.Add(this.button11);
+            this.Controls.Add(this.btnComoestas);
+            this.Controls.Add(this.btnBien);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
@@ -366,7 +366,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnHoy);
+            this.Controls.Add(this.btnHola);
             this.Controls.Add(this.btnVolver);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -390,7 +390,7 @@
         private System.Windows.Forms.Button btnLecciones;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Button btnHoy;
+        private System.Windows.Forms.Button btnHola;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
@@ -401,7 +401,7 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button11;
-        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button btnBien;
+        private System.Windows.Forms.Button btnComoestas;
     }
 }
