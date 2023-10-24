4a0f1a9095672783e7825c40a1dae6c944eb2be0
manci <manci@LAPTOP-S0L1KC73>
2023-10-23 20:15:44 -0600

    Add color forms

--- Lenguaje de señas App/Triste.Designer.cs
+++ Lenguaje de señas App/Triste.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Triste
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(415, 450);
+            this.Name = "Triste";
             this.Text = "Triste";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
