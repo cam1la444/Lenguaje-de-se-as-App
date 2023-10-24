891cb6c9cc0d59ed7f0cae0be1450928cfcbc06b
manci <manci@LAPTOP-S0L1KC73>
2023-10-23 19:49:55 -0600

    Add btn para form de adjetivos

--- Lenguaje de señas App/Adjetivos.Designer.cs
+++ Lenguaje de señas App/Adjetivos.Designer.cs
@@ -36,6 +36,18 @@
             this.btnLecciones = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnFeo = new System.Windows.Forms.Button();
+            this.btnBajo = new System.Windows.Forms.Button();
+            this.btnDebil = new System.Windows.Forms.Button();
+            this.btntriste = new System.Windows.Forms.Button();
+            this.btnInteligente = new System.Windows.Forms.Button();
+            this.btnFuerte = new System.Windows.Forms.Button();
+            this.btnAlegre = new System.Windows.Forms.Button();
+            this.btnEnojado = new System.Windows.Forms.Button();
+            this.btnAlto = new System.Windows.Forms.Button();
+            this.btnDelgado = new System.Windows.Forms.Button();
+            this.btnGordo = new System.Windows.Forms.Button();
+            this.btnBonito = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -142,11 +154,203 @@
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
+            // btnFeo
+            // 
+            this.btnFeo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFeo.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnFeo.FlatAppearance.BorderSize = 2;
+            this.btnFeo.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnFeo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFeo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnFeo.Location = new System.Drawing.Point(30, 77);
+            this.btnFeo.Name = "btnFeo";
+            this.btnFeo.Size = new System.Drawing.Size(135, 51);
+            this.btnFeo.TabIndex = 78;
+            this.btnFeo.Text = "Feo";
+            this.btnFeo.UseVisualStyleBackColor = true;
+            // 
+            // btnBajo
+            // 
+            this.btnBajo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBajo.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnBajo.FlatAppearance.BorderSize = 2;
+            this.btnBajo.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnBajo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBajo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnBajo.Location = new System.Drawing.Point(309, 168);
+            this.btnBajo.Name = "btnBajo";
+            this.btnBajo.Size = new System.Drawing.Size(135, 51);
+            this.btnBajo.TabIndex = 79;
+            this.btnBajo.Text = "Bajo";
+            this.btnBajo.UseVisualStyleBackColor = true;
+            // 
+            // btnDebil
+            // 
+            this.btnDebil.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDebil.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnDebil.FlatAppearance.BorderSize = 2;
+            this.btnDebil.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnDebil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDebil.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnDebil.Location = new System.Drawing.Point(297, 364);
+            this.btnDebil.Name = "btnDebil";
+            this.btnDebil.Size = new System.Drawing.Size(135, 51);
+            this.btnDebil.TabIndex = 80;
+            this.btnDebil.Text = "Debil";
+            this.btnDebil.UseVisualStyleBackColor = true;
+            // 
+            // btntriste
+            // 
+            this.btntriste.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btntriste.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btntriste.FlatAppearance.BorderSize = 2;
+            this.btntriste.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btntriste.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btntriste.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btntriste.Location = new System.Drawing.Point(199, 272);
+            this.btntriste.Name = "btntriste";
+            this.btntriste.Size = new System.Drawing.Size(135, 51);
+            this.btntriste.TabIndex = 81;
+            this.btntriste.Text = "Triste";
+            this.btntriste.UseVisualStyleBackColor = true;
+            // 
+            // btnInteligente
+            // 
+            this.btnInteligente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnInteligente.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnInteligente.FlatAppearance.BorderSize = 2;
+            this.btnInteligente.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnInteligente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInteligente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnInteligente.Location = new System.Drawing.Point(392, 272);
+            this.btnInteligente.Name = "btnInteligente";
+            this.btnInteligente.Size = new System.Drawing.Size(135, 51);
+            this.btnInteligente.TabIndex = 82;
+            this.btnInteligente.Text = "Inteligente";
+            this.btnInteligente.UseVisualStyleBackColor = true;
+            // 
+            // btnFuerte
+            // 
+            this.btnFuerte.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFuerte.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnFuerte.FlatAppearance.BorderSize = 2;
+            this.btnFuerte.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnFuerte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFuerte.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnFuerte.Location = new System.Drawing.Point(603, 272);
+            this.btnFuerte.Name = "btnFuerte";
+            this.btnFuerte.Size = new System.Drawing.Size(135, 51);
+            this.btnFuerte.TabIndex = 83;
+            this.btnFuerte.Text = "Fuerte";
+            this.btnFuerte.UseVisualStyleBackColor = true;
+            // 
+            // btnAlegre
+            // 
+            this.btnAlegre.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAlegre.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnAlegre.FlatAppearance.BorderSize = 2;
+            this.btnAlegre.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnAlegre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlegre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnAlegre.Location = new System.Drawing.Point(504, 168);
+            this.btnAlegre.Name = "btnAlegre";
+            this.btnAlegre.Size = new System.Drawing.Size(135, 51);
+            this.btnAlegre.TabIndex = 84;
+            this.btnAlegre.Text = "Alegre";
+            this.btnAlegre.UseVisualStyleBackColor = true;
+            // 
+            // btnEnojado
+            // 
+            this.btnEnojado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEnojado.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnEnojado.FlatAppearance.BorderSize = 2;
+            this.btnEnojado.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnEnojado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnojado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnEnojado.Location = new System.Drawing.Point(30, 272);
+            this.btnEnojado.Name = "btnEnojado";
+            this.btnEnojado.Size = new System.Drawing.Size(135, 51);
+            this.btnEnojado.TabIndex = 85;
+            this.btnEnojado.Text = "Enojado";
+            this.btnEnojado.UseVisualStyleBackColor = true;
+            // 
+            // btnAlto
+            // 
+            this.btnAlto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAlto.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnAlto.FlatAppearance.BorderSize = 2;
+            this.btnAlto.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnAlto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnAlto.Location = new System.Drawing.Point(117, 168);
+            this.btnAlto.Name = "btnAlto";
+            this.btnAlto.Size = new System.Drawing.Size(135, 51);
+            this.btnAlto.TabIndex = 86;
+            this.btnAlto.Text = "Alto";
+            this.btnAlto.UseVisualStyleBackColor = true;
+            // 
+            // btnDelgado
+            // 
+            this.btnDelgado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelgado.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnDelgado.FlatAppearance.BorderSize = 2;
+            this.btnDelgado.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnDelgado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelgado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnDelgado.Location = new System.Drawing.Point(603, 77);
+            this.btnDelgado.Name = "btnDelgado";
+            this.btnDelgado.Size = new System.Drawing.Size(135, 51);
+            this.btnDelgado.TabIndex = 87;
+            this.btnDelgado.Text = "Delgado";
+            this.btnDelgado.UseVisualStyleBackColor = true;
+            // 
+            // btnGordo
+            // 
+            this.btnGordo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGordo.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnGordo.FlatAppearance.BorderSize = 2;
+            this.btnGordo.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnGordo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGordo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnGordo.Location = new System.Drawing.Point(392, 77);
+            this.btnGordo.Name = "btnGordo";
+            this.btnGordo.Size = new System.Drawing.Size(135, 51);
+            this.btnGordo.TabIndex = 88;
+            this.btnGordo.Text = "Gordo";
+            this.btnGordo.UseVisualStyleBackColor = true;
+            // 
+            // btnBonito
+            // 
+            this.btnBonito.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBonito.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue;
+            this.btnBonito.FlatAppearance.BorderSize = 2;
+            this.btnBonito.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnBonito.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBonito.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnBonito.Location = new System.Drawing.Point(199, 77);
+            this.btnBonito.Name = "btnBonito";
+            this.btnBonito.Size = new System.Drawing.Size(135, 51);
+            this.btnBonito.TabIndex = 89;
+            this.btnBonito.Text = "Bonito";
+            this.btnBonito.UseVisualStyleBackColor = true;
+            // 
             // Adjetivos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(790, 442);
+            this.Controls.Add(this.btnBonito);
+            this.Controls.Add(this.btnGordo);
+            this.Controls.Add(this.btnDelgado);
+            this.Controls.Add(this.btnAlto);
+            this.Controls.Add(this.btnEnojado);
+            this.Controls.Add(this.btnAlegre);
+            this.Controls.Add(this.btnFuerte);
+            this.Controls.Add(this.btnInteligente);
+            this.Controls.Add(this.btntriste);
+            this.Controls.Add(this.btnDebil);
+            this.Controls.Add(this.btnBajo);
+            this.Controls.Add(this.btnFeo);
             this.Controls.Add(this.btnVolver);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -170,5 +374,17 @@
         private System.Windows.Forms.Button btnLecciones;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnFeo;
+        private System.Windows.Forms.Button btnBajo;
+        private System.Windows.Forms.Button btnDebil;
+        private System.Windows.Forms.Button btntriste;
+        private System.Windows.Forms.Button btnInteligente;
+        private System.Windows.Forms.Button btnFuerte;
+        private System.Windows.Forms.Button btnAlegre;
+        private System.Windows.Forms.Button btnEnojado;
+        private System.Windows.Forms.Button btnAlto;
+        private System.Windows.Forms.Button btnDelgado;
+        private System.Windows.Forms.Button btnGordo;
+        private System.Windows.Forms.Button btnBonito;
     }
 }
