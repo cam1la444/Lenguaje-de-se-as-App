12a0cfb75cbccb620aeba6d78fb62599d442a02f
manci <manci@LAPTOP-S0L1KC73>
2023-11-04 17:52:38 -0600

    Agregue codigo de conexion

---
+++ Lenguaje de señas App/ConexionBaseDatos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lenguaje_de_señas_App
+{
+    class ConexionBaseDatos
+    {
+        public static int CrearCuentas(string Email, string Clave)
+        {
+            int resultado = 0;
+            SqlConnection conectar = ConexionBaseDatos.ObtenerConexion();
+
+            SqlCommand Comando = new SqlCommand(string.Format("INSERT INTO registroUsuarios (Email,Clave)"));
+
+            resultado = Comando.ExecuteNonQuery();
+            conectar.Close();
+
+            return resultado;
+        }
+    }
+}
