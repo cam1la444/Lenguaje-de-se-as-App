cabdbf3683d30800048f35cf021f6095a29ae791
manci <manci@LAPTOP-S0L1KC73>
2023-11-04 14:45:55 -0600

    Agregue una clase para conexion

---
+++ Lenguaje de señas App/ConexionBaseDatos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lenguaje_de_señas_App
+{
+    internal class ConexionBaseDatos
+    {
+        public static SqlConection = 
+    }
+}
