781f5b03982553d0507b9fec7733415008964e42
manci <manci@LAPTOP-S0L1KC73>
2023-11-04 22:23:47 -0600

    Agregue mas codigo

--- Lenguaje de señas App/Conexion.cs
+++ Lenguaje de señas App/Conexion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,12 @@ namespace Lenguaje_de_señas_App
     {
         public static int Agregar(Usuarios pUsuarios)
         {
+            using (SqlConnection conectar = BDUsuarios.ObtenerConexion())
+            {
+                SqlCommand Comando = new SqlCommand(string.Format("INSERT INTO registroUsuarios (Email,Clave) VALUES ('{0}','{1}')", pEmail.Email,pClave.Clave),conectar);
 
+                return = Comando.ExecuteNonQuery();
+            }
         }
     }
 }
