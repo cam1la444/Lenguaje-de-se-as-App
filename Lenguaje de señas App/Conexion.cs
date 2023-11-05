59fd2fe7bac0897ef5fc67f834efee8d35f724ed
manci <manci@LAPTOP-S0L1KC73>
2023-11-05 10:01:51 -0600

    borre dos parentesis

--- Lenguaje de señas App/Conexion.cs
+++ Lenguaje de señas App/Conexion.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -14,7 +14,7 @@ namespace Lenguaje_de_señas_App
             int retorno =  0;
             using (SqlConnection conectar = BDUsuarios.ObtenerConexion())
             {
-                SqlCommand Comando = new SqlCommand(string.Format("INSERT INTO registroUsuarios (Email,Clave) VALUES ('{0}','{1}')", pUsuarios.Email, pUsuarios.Clave), conectar); ))
+                SqlCommand Comando = new SqlCommand(string.Format("INSERT INTO registroUsuarios (Email,Clave) VALUES ('{0}','{1}')", pUsuarios.Email, pUsuarios.Clave), conectar);
                 retorno = Comando.ExecuteNonQuery();
             }
             return retorno;
