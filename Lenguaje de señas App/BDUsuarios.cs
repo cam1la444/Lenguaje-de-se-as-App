99887a174543fc1fbfb388573a1d7290b05ad74d
manci <manci@LAPTOP-S0L1KC73>
2023-11-04 18:58:41 -0600

    Agregue mas codigo para coneccion

--- Lenguaje de señas App/BDUsuarios.cs
+++ Lenguaje de señas App/BDUsuarios.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,12 @@ namespace Lenguaje_de_señas_App
 {
     internal class BDUsuarios
     {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conectar = new SqlConnection("Data Source=LAPTOP-S0L1KC73\\SQLMANCIA; Initial Catalog=registrarUsuario");
+            conectar.Open();
 
+            return conectar;
+        }
     }
 }
