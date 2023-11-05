dd5a6f08fb20385f36e1c3abb7a8564aae9c9cc2
manci <manci@LAPTOP-S0L1KC73>
2023-11-04 20:11:21 -0600

    cod de agregar

---
+++ Lenguaje de señas App/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lenguaje_de_señas_App
+{
+    internal class Conexion
+    {
+        public static int Agregar(Usuarios pUsuarios)
+        {
+
+        }
+    }
+}
